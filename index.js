1316b908fb7fd6601f41cd2ace0273514287c071
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-06-09 15:08:00 -0600

    commit tree b8f27e389ffc3a277fb57621265380b151f6d1a5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin(array) {
+    let winYear;
+    array.find(object => {
+      if (object.result === 'W') {
+          winYear = object.year;
+      }
+    })
+    return winYear;
+}
 
